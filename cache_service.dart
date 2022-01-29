7678e2b4f11dbc1bb44405c0bf7ae7fd3035a26b
Huseyin Bedir <57250424+huseyinbedir@users.noreply.github.com>
2022-01-29 11:33:56 +0300

    Create cache_service.dart

---
+++ cache_service.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:account_app/model/base_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CacheService {
+  static CacheService? _instance;
+  static CacheService get instance {
+    if (_instance != null) return _instance!;
+
+    _instance = CacheService._init();
+    return _instance!;
+  }
+
+  late SharedPreferences prefs;
+  CacheService._init();
+
+  Future<void> initPrefences() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  Future<bool> saveCacheItem<T>(String key, List<T> model) async {
+    final _stringModel = jsonEncode(model);
+    return await prefs.setString(key, _stringModel);
+  }
+
+  Future<bool> saveCache(String key, dynamic value) async {
+    return await prefs.setString(key, value);
+  }
+
+  String getCache(String key) {
+    return prefs.getString(key) ?? "";
+  }
+
+  Future<bool> setBoolCache(String key, bool value) {
+    return prefs.setBool(key, value);
+  }
+
+  bool getBoolCache(String key) {
+    return prefs.getBool(key) ?? false;
+  }
+
+  Future<bool> removeCacheItem<T>(String key) async {
+    return await prefs.remove(key);
+  }
+
+  Future<T> getCacheItem<T extends IBaseModel>(
+      String key, IBaseModel model) async {
+    final cacheData = prefs.getString(key) ?? '';
+    final otherModel = jsonDecode(cacheData);
+
+    return model.fromJson(otherModel) as T;
+  }
+
+  Future<bool> saveCacheList<T>(String key, List<T> model) async {
+    final _stringModel = jsonEncode(model);
+    return await prefs.setString(key, _stringModel);
+  }
+
+  List<T> getCacheList<T extends IBaseModel>(String key, T model) {
+    final cacheData = prefs.getString(key) ?? "";
+    if (cacheData.isNotEmpty) {
+      return model.fromJson(cacheData).toList() as List<T>;
+    }
+    return [];
+  }
+
+  Future<bool> removeAllCache() async {
+    return await prefs.clear();
+  }
+}
